eead18d62577d4f18127c71768b0a196174e03e1
Jovana Carvalho <109672630+Jvbcarv@users.noreply.github.com>
2023-06-23 20:06:41 -0300

    Add files via upload

---
+++ Exercício 6.py
@@ -0,0 +1,9 @@
+#utilizando For
+for i in range(1, 11):
+  print('3 x {} = {}'.format(i, 3 * i))
+
+#utilizando While:
+numero = 1
+while numero <= 10:
+  print('3 x {} = {}'.format(numero, 3 * numero))
+  numero += 1
